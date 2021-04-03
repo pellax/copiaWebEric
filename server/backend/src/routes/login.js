a4d948acd8b052fdff5cefb52c32eef7ee4fbd80
eric.ruiz.gimenez <eric.ruiz.gimenez@estudiantat.upc.edu>
2021-04-03 15:59:40 +0200

    Login simple

--- server/backend/src/routes/login.js
+++ server/backend/src/routes/login.js
@@ -23,4 +23,64 @@ router.post('/login', async (req,res) => {
 
 })
 
+router.get('/tasks', (req,res)=>{
+    res.json([
+        {
+            _id:1,
+            name: 'Task one',
+            description: 'lorem ipsum',
+            date: "2021-04-03T11:52:41.220Z"
+        },
+        {
+            _id:2,
+            name: 'Task two',
+            description: 'lorem ipsum',
+            date: "2021-04-03T11:52:41.220Z"
+        },
+        {
+            _id:3,
+            name: 'Task three',
+            description: 'lorem ipsum',
+            date: "2021-04-03T11:52:41.220Z"
+        },
+    ])
+})
+
+router.get('/private-tasks', verifyToken, (req,res)=>{
+    res.json([
+        {
+            _id:1,
+            name: 'Task one',
+            description: 'lorem ipsum',
+            date: "2021-04-03T11:52:41.220Z"
+        },
+        {
+            _id:2,
+            name: 'Task two',
+            description: 'lorem ipsum',
+            date: "2021-04-03T11:52:41.220Z"
+        },
+        {
+            _id:3,
+            name: 'Task three',
+            description: 'lorem ipsum',
+            date: "2021-04-03T11:52:41.220Z"
+        },
+    ])
+})
+
+function verifyToken(req, res, next){
+    if (!req.headers.authorization){
+        return res.status(401).send("Unauthorized Request");
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    if (token == null) return res.status(401).send("Unauthorized Request");
+    const payload = jwt.verify(token, 'secretKey')
+    req.userID = payload._id;
+    next();
+}
+
+router.get('/profile', verifyToken, (req,res) =>{
+    res.send(req.userID);
+})
 module.exports = router;
