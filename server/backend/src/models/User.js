96c974032d1b08395c791ad371e8d5470cd0d033
pellax <tuxisalive@gmail.com>
2021-04-29 12:12:20 +0200

    passport en proceso

--- server/backend/src/models/User.js
+++ server/backend/src/models/User.js
@@ -43,5 +43,17 @@ var bcrypt = require('bcrypt');
          callback(undefined, isMatch);
      });
  };
+
+ userSchema.methods.generateJwt = function() {
+    var expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+  
+    return jwt.sign({
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+      exp: parseInt(expiry.getTime() / 1000),
+    }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
+  };
  
  module.exports = model('User', userSchema);
